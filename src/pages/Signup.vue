a28d34c5f00c68e5c79bd812836a58b8f4c94b01
Keisuke Taniguchi <rememberthatk@icloud.com>
2022-04-02 08:36:10 +0900

    add signup

---
+++ src/pages/Signup.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="p-signup flex justify-center items-center flex-none bg-gray-50 h-screen"
+  >
+    <form class="bg-white p-4 h-fit rounded-md">
+      <h1 class="text-3xl font-bold text-center mb-4">Signup</h1>
+      <div class="mb-6">
+        <label
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          for="email"
+          >Your email</label
+        >
+        <input
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@flowbite.com"
+          required
+          type="email"
+        />
+      </div>
+      <div class="mb-6">
+        <label
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          for="password"
+          >Your password</label
+        >
+        <input
+          id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+          type="password"
+        />
+      </div>
+      <div class="flex items-start mb-6">
+        <div class="flex items-center h-5">
+          <input
+            id="remember"
+            aria-describedby="remember"
+            class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+            required
+            type="checkbox"
+          />
+        </div>
+        <div class="ml-3 text-sm">
+          <label
+            class="font-medium text-gray-900 dark:text-gray-300"
+            for="remember"
+            >Remember me</label
+          >
+        </div>
+      </div>
+      <button
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.p-signup {
+  form {
+    width: 600px;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
-import Home from "@/pages/Home.vue";
-import { createRouter, createWebHistory } from "vue-router";
 import About from "@/pages/About.vue";
+import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
+import Signup from "@/pages/Signup.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/login", name: "Login", component: Login },
+  { path: "/signup", name: "Signup", component: Signup },
   { path: "/about", name: "About", component: About },
 ];
 
