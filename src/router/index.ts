66c78092640e4d057443d3ad56e9455f84047e79
Keisuke Taniguchi <rememberthatk@icloud.com>
2022-04-02 08:32:57 +0900

    add Login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import Home from "@/pages/Home.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import About from "@/pages/About.vue";
+import Login from "@/pages/Login.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
+  { path: "/login", name: "Login", component: Login },
   { path: "/about", name: "About", component: About },
 ];
 
